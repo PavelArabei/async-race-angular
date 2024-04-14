93987080a081d57c8b05e8a8ef2184d9743c7432
Pashtet <ptashkaaaaaa@gmail.com>
2024-04-14 17:23:35 +0200

    feat: enhance header layout and button component with new styles and SVG color customization

--- src/app/core/components/button/button.component.ts
+++ src/app/core/components/button/button.component.ts
@@ -1,13 +1,15 @@
+import { NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, NgStyle],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
   @Input() svgName: string | null = null;
+  @Input() svgColor: string | null = null;
 }
