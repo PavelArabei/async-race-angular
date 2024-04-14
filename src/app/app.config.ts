1ef2f4d8a750464c9e975b996611cc18b63ce2a3
Pashtet <ptashkaaaaaa@gmail.com>
2024-04-14 22:21:22 +0200

    feat: integrate NgRx for state management, add store, effects, and devtools configurations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { mainUrlInterceptor } from '@core/interceptors/main-url.interceptor';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
 
@@ -11,5 +14,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([mainUrlInterceptor])),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
