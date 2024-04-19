3062d36bcb1ba3a9d0f8848b0af4e9b01a230233
Pashtet <ptashkaaaaaa@gmail.com>
2024-04-19 15:44:12 +0200

    feat: implement OnInit and resize handling in RaceComponent, integrate ResizeEmitterService for dynamic distance tracking

--- src/app/garage/components/race/race.component.ts
+++ src/app/garage/components/race/race.component.ts
@@ -1,10 +1,12 @@
 import { NgStyle, TitleCasePipe } from '@angular/common';
-import { Component, inject, Input } from '@angular/core';
+import { Component, DestroyRef, Input, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
 import { Car } from '@app/shared/types/car';
 import { ButtonComponent } from '@core/components/button/button.component';
 import { GarageHttpActions } from '@garage/redux/actions/garageHttpActions';
+import { ResizeEmitterService } from '@garage/services/resize-emitter/resize-emitter.service';
 import { UpdateCarService } from '@garage/services/update-car/update-car.service';
 import { Store } from '@ngrx/store';
 
@@ -15,11 +17,17 @@ import { Store } from '@ngrx/store';
   templateUrl: './race.component.html',
   styleUrl: './race.component.scss',
 })
-export class RaceComponent {
+export class RaceComponent implements OnInit {
   @Input({ required: true }) car!: Car;
 
-  private readonly store: Store = inject(Store);
-  private readonly updateCarService = inject(UpdateCarService);
+  private distance = 0;
+
+  constructor(
+    private readonly store: Store,
+    private readonly updateCarService: UpdateCarService,
+    private readonly resizeEmitter: ResizeEmitterService,
+    private readonly destroyRef: DestroyRef
+  ) {}
 
   removeCar() {
     this.store.dispatch(GarageHttpActions.deleteCar({ id: this.car.id }));
@@ -27,5 +35,17 @@ export class RaceComponent {
 
   selectCar() {
     this.updateCarService.selectCar(this.car);
+  }
+
+  ngOnInit(): void {
+    this.getDistance();
+  }
+
+  private getDistance() {
+    this.resizeEmitter.carDistance
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((distance) => {
+        this.distance = distance;
+      });
   }
 }
