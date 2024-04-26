aca24331a3a0a09c15b43947607cf069a6fdb069
Pashtet <ptashkaaaaaa@gmail.com>
2024-04-26 19:54:47 +0200

    feat: hide footer and header when app in incorrect route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from '@core/components/footer/footer.component';
 import { HeaderComponent } from '@core/components/header/header.component';
 import { IconRegisterService } from '@core/services/icon-register.service';
+import { RouterRoutes } from '@utils/constants/router-routes';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +15,26 @@ import { IconRegisterService } from '@core/services/icon-register.service';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
+  isCorrectRoute = false;
   private readonly iconRegisterService = inject(IconRegisterService);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
 
   public ngOnInit(): void {
     this.iconRegisterService.registerIcons();
+    this.checkCurrentRoute();
+  }
+
+  private checkCurrentRoute(): void {
+    const { WINNERS, GARAGE } = RouterRoutes;
+    const availableRoutes = [WINNERS, GARAGE] as string[];
+    this.router.events
+      .pipe(
+        filter((event) => event.constructor.name === 'NavigationEnd'),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(() => {
+        this.isCorrectRoute = availableRoutes.includes(this.router.url.slice(1));
+      });
   }
 }
